a086cc299c8a9df25af3890eecec1fae880b69f4
Andrew <andrei.zaitcev81@gmail.com>
2025-09-21 14:20:38 +0300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	transactions := make([]float64, 0, 30)
+	for {
+		fmt.Println("Здравствуйте, скики вы сегодня потратили ? Напишите число > ")
+		coastUser := getScanTransaction()
+
+		if coastUser <= 0 {
+			break
+		}
+
+		transactions = append(transactions, coastUser)
+
+		fmt.Println("Ваша общаяя сумма трат:", addingSum(transactions))
+	}
+}
+
+func addingSum(transactions []float64) float64 {
+	var sum float64
+	for _, value := range transactions {
+		sum += value
+	}
+	return sum
+}
+
+func getScanTransaction() float64 {
+	var coastUser float64
+
+	fmt.Scan(&coastUser)
+	return coastUser
 }
